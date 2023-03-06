1deb3912d440df838c814915a6d33ad462f073bb
felipesa97 <126787751+felipesa97@users.noreply.github.com>
2023-03-06 14:32:17 -0300

    Add files via upload

---
+++ print('Hello, World!').py
@@ -0,0 +1,24 @@
+# Hello, World!
+print('Hello, World!')
+# program.py
+sum = 1+2
+print(sum)
+print("show this in the console")
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+# <left side> <operator> <right side>
+left_side = 10
+right_side = 5
+left_side / right_side # 2
+from datetime import date
+date.today()
+print(date.today())
+# argumentos en linea de comando
+import sys
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) #first arg
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
